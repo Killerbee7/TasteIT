69dd5620bf5d4a9705097031f94a7b59ee4669f7
Dibya Dahal <102875464+Killerbee7@users.noreply.github.com>
2023-01-15 20:50:21 +0200

    some media screen fixes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React from "react";
+import classes from "./About.module.css"
 
 const About = () => {
-    return (
-        <div>
-           <h1>TasteIT</h1> 
-           <p>This is a project for react by Margit Tennosaar, Business College Helsinki. <a target="_blank" rel="noopener noreferrer" href="https://github.com/Killerbee7/TasteIT">Click to be redirected to github repo</a></p>
-        </div>
-    );
+  return (
+    <div className={classes.about}>
+      <h1>TasteIT- Built on React</h1>
+      <h4>
+        This is a project for react by Margit Tennosaar, Business College
+        Helsinki.User can add recipe from their country and also, browse recipes
+        from the database.{" "}
+      </h4>
+      <h3>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/Killerbee7/TasteIT"
+        >
+          Click to be redirected to github repo
+        </a>
+      </h3>
+      <h3>To run the app </h3>
+      <p>npm i (to install node modules)</p>
+      <p>npm start</p>
+      <p>npm run server(in new terminal)</p>
+      <h3>Build using:</h3>* HTML * CSS * React JS * Node
+    </div>
+  );
 };
 
 export default About;
