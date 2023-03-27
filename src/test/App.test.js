e8b433ce57c8af0d39d4faf9e400d66e75e35562
Dibya Dahal <102875464+Killerbee7@users.noreply.github.com>
2023-03-27 15:19:07 +0300

    unit test task 1 continued

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,1 +1,22 @@
-jest.mock
+import { render, screen } from "@testing-library/react";
+import RecipeForm from "../components/RecipeForm";
+import '@testing-library/jest-dom/extend-expect'
+
+describe(RecipeForm,() => {
+    it("form displays all labels and inputs", ()=>{
+        const {} = render(<RecipeForm 
+        name="" 
+        id=""
+        author= ""
+        desc= ""
+        country= ""
+        flag=""
+        img= ""
+        inst= ""
+        />)
+
+        const labeltext = screen.getByLabelText(/name/i);
+       expect(labeltext).toBeInTheDocument()
+    });
+
+})
